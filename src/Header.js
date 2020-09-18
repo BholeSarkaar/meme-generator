b77cb592493a336b30f004d26a0260afa95e1707
Bhole Sarkaar <naikaderohit833@gmail.com>
2020-09-18 23:11:21 +0530

    development completed

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-
-class Header extends Component{
-    render(){
-        return(
+import { Container, Row, Col } from 'react-bootstrap';
+class Header extends Component {
+    render() {
+        return (
             <header>
-                <img 
-                    src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
-                    alt="Problem?"
-                />
-                <p>Meme Generator</p>
+                <Container>
+                    <Row>
+                        <Col className="col-md-3 col-sm-1">
+                            <img
+                                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+                                alt="Problem?"
+                                height="100px"
+                                width="100px"
+                            />
+                        </Col>
+                        <Col className="col-md-9 col-sm-11 ml-0">
+                            <h1 className="p ml-0 col-md-9">Meme Generator</h1>
+                        </Col>
+                    </Row>
+
+                </Container>
+
+
             </header>
         )
     }
--- src/memegenerator.js
+++ src/memegenerator.js
@@ -1,59 +1,73 @@
 import React, { Component } from 'react'
-
-class Meme extends Component{
-    constructor(){
+import { Container, Row, Card, Col, FormControl, Form, Button } from 'react-bootstrap';
+class Meme extends Component {
+    constructor() {
         super()
-        this.state={
-            topText:"",
-            bottomText:"",
-            randomImg:"http://i.imgflip.com/1bij.jpg",
-            AllMemes:[]
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            AllMemes: []
         }
-        this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch("https://api.imgflip.com/get_memes")
-        .then(response=>response.json())
-        .then(response => {
-            const {memes} = response.data
-            this.setState({ AllMemes: memes })
-        })
+            .then(response => response.json())
+            .then(response => {
+                const { memes } = response.data
+                this.setState({ AllMemes: memes })
+            })
     }
-    handleChange(event){
-        const {name,value}=event.target
-        this.setState({[name]:value})
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
     }
-    handleSubmit(event){
+    handleSubmit(event) {
         event.preventDefault();
-        const randNum=Math.floor(Math.random()*this.state.AllMemes.length);
-        const randomMemeImg=this.state.AllMemes[randNum].url;
-        this.setState({randomImg:randomMemeImg});
+        const randNum = Math.floor(Math.random() * this.state.AllMemes.length);
+        const randomMemeImg = this.state.AllMemes[randNum].url;
+        this.setState({ randomImg: randomMemeImg });
     }
-    render(){
-        return(
-            <div>
-                <form className='meme-form' onSubmit={this.handleSubmit}>
-                    <input 
-                       type="text"
-                       name="topText"
-                       placeholder="Top text"
-                       value={this.state.topText}
-                       onChange={this.handleChange}/>
-                    <input 
-                       type="text"
-                       name="bottomText"
-                       placeholder="Bottom text"
-                       value={this.state.bottomText}
-                       onChange={this.handleChange}/>
-                       <button>Generate</button>
-                </form>
-                <div className="meme">
-                    <img src={this.state.randomImg} />
-                    <h2 className="top">{this.state.topText}</h2>
-                    <h2 className="bottom">{this.state.bottomText}</h2>
-                </div>
-            </div>
+    render() {
+        return (
+            <Container fluid>
+                <Row className="d-flex justify-content-md-center">
+                    <Card className="mt-5 col-md-4">
+                        <Form onSubmit={this.handleSubmit}>
+                            <Row>
+                                <Col className="d-flex justify-content-md-center col-md-6 mt-5">
+                                    <FormControl
+                                        type="text"
+                                        name="topText"
+                                        placeholder="Top text"
+                                        value={this.state.topText}
+                                        onChange={this.handleChange} />
+                                </Col>
+                                <Col className="d-flex justify-content-md-center col-md-6 mt-5">
+                                    <FormControl
+                                        type="text"
+                                        name="bottomText"
+                                        placeholder="Bottom text"
+                                        value={this.state.bottomText}
+                                        onChange={this.handleChange} />
+                                </Col>
+                                <Col className="d-flex justify-content-md-center col-sm-12 mt-4">
+                                    <Button >Generate Meme.</Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="d-flex justify-content-md-center mt-4 meme">
+                                    <img src={this.state.randomImg}/>
+                                    <h2 className="top">{this.state.topText}</h2>
+                                    <h2 className="bottom">{this.state.bottomText}</h2>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Card>
+                </Row>
+            </Container>
         )
     }
 }
